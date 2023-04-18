ef57289518863a8539ad5a818da80d0e9f139b57
ahasoplakus <sukalposaha@gmail.com>
2023-04-18 16:58:15 +0530

    fix ci

--- R/app_ui.R
+++ R/app_ui.R
@@ -2,7 +2,10 @@
 #'
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
-#' @import shiny bs4Dash
+#' @import shiny
+#' @importFrom bs4Dash dashboardPage dashboardHeader dashboardSidebar
+#' sidebarMenu menuItem menuItemOutput renderMenu dashboardBody tabBox box
+#' boxSidebar
 #' @noRd
 app_ui <- function(request) {
   tagList(
