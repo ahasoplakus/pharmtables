6165a13ed028a188c99d46806d0f81245637c971
ahasoplakus <sukalposaha@gmail.com>
2023-05-25 14:22:40 +0530

    binary rsconnect

--- deploy/deploy-clinTables.R
+++
@@ -1,8 +0,0 @@
-# deploy/deploy-shinyapps.R
-# usethis::use_build_ignore("deploy")
-rsconnect::setAccountInfo(
-  Sys.getenv("SHINYAPPS_ACCOUNT"),
-  Sys.getenv("SHINYAPPS_TOKEN"),
-  Sys.getenv("SHINYAPPS_SECRET")
-)
-rsconnect::deployApp(appName = "clinTables") # or "CI"
